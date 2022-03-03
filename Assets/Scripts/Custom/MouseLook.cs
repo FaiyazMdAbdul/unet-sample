97091d8191bd3518292d516f272693901bb2412f
Faiyaz <mail25yaz@gmail.com>
2022-03-03 13:42:28 +0530

    Initial commit

---
+++ Assets/Scripts/Custom/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float lookSpeed = 3;
+    private Vector2 rotation = new Vector2(15,160);
+    public Transform hacker;
+    private void Start()
+    {
+        Camera.main.transform.LookAt(hacker);
+    }
+    public void LookX() // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        rotation.x = Mathf.Clamp(rotation.x, -30f, 30f);
+       // rotation.y = Mathf.Clamp(rotation.y, -60f, 60f);
+
+        transform.eulerAngles = new Vector2(rotation.x, rotation.y) * lookSpeed;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, rotation.y, 0);
+    }
+  
+
+
+private void Update()
+    {
+        LookX();
+        
+    }
+}
---
+++ Assets/Scripts/Custom/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Playables;
+
+public class UIManager : MonoBehaviour
+{
+    NetworkManager manager;
+
+    [SerializeField] GameObject loginPanel;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<NetworkManager>();
+        loginPanel.SetActive(true);
+    }
+
+    public void Create()
+    {
+        manager.StartHost();
+        loginPanel.SetActive(false);
+    }
+
+    public void Join()
+    {
+        manager.StartClient();
+        loginPanel.SetActive(false);
+    }
+
+   [SerializeField] PlayableDirector shootTimeline;
+
+    [SerializeField] Animator animator;
+    public void ShootSequence()
+    {
+        animator.gameObject.GetComponent<NetworkAnimator>().SetTrigger("shoot");
+        //animator.SetTrigger("shoot");
+    }
+
+}
